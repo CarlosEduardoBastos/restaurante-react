e32a5f261231bbaab15caccae90c4e6b77c007f3
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2021-03-19 10:27:22 -0300

    adicionando o estilo do header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './css/Header.css'
 
 export default function Header(){
     return (
         <header className="header_color">
             <section className="header_corpo">
-                <Link to="/">logo</Link>
+                <Link to="/" className="logo">logo</Link>
                 <nav className="menu">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="sobre">Sobre</NavLink>
-                    <NavLink to="menu">Menu</NavLink>
-                    <NavLink to="login">Login</NavLink>
+                    <NavLink to="/" className="links" end>Home</NavLink>
+                    <NavLink to="sobre" className="links">Sobre</NavLink>
+                    <NavLink to="menu" className="links">Menu</NavLink>
+                    <NavLink to="login" className="links">Login</NavLink>
                 </nav>
             </section>
         </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className="corpo">
+            <div className="banner"></div>
+        </section>
     )
 }
 
