fb69ccdcef4b1c4a460efd2bbfb632328d55229d
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2021-03-19 09:41:38 -0300

    init project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header(){
+    return (
+        <header className="header_color">
+            <section className="header_corpo">
+                
+            </section>
+        </header>
+    )
+}
