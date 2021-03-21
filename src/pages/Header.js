7ec8dc979d868d0d2192859b3827c245079c346c
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2021-03-21 08:02:39 -0300

    criando home

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import './css/Header.css'
+import styles from './css/Header.module.css'
 
 export default function Header(){
     return (
-        <header className="header_color">
-            <section className="header_corpo">
-                <Link to="/" className="logo">logo</Link>
+        <header className={styles.header_color}>
+            <section className={styles.header_corpo}>
+                <Link to="/" className={styles.logo}>logo</Link>
                 <nav className="menu">
-                    <NavLink to="/" className="links" end>Home</NavLink>
-                    <NavLink to="sobre" className="links">Sobre</NavLink>
-                    <NavLink to="menu" className="links">Menu</NavLink>
-                    <NavLink to="login" className="links">Login</NavLink>
+                    <ul>
+                        <li><NavLink to="/" activeClassName={styles.active} className={styles.links} end>Home</NavLink></li>
+                        <li><NavLink to="sobre" activeClassName={styles.active} className={styles.links}>Sobre</NavLink></li>
+                        <li><NavLink to="menu" activeClassName={styles.active} className={styles.links}>Menu</NavLink></li>
+                        <li><NavLink to="login" activeClassName={styles.active} className={styles.links}>Login</NavLink></li>
+                    </ul>
                 </nav>
             </section>
         </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './css/Home.module.css';
+import foto1 from '../img/pexels-deeana-creates-2565222.jpg'
+import foto2 from '../img/pexels-cottonbro-4254279.jpg'
 const Home = () => {
     return (
-        <section className="corpo">
-            <div className="banner"></div>
+        <section className={styles.corpo}>
+            <div className={styles.banner}></div>
+            <section className={styles.content}>
+                <section className={styles.content_corpo}>
+                    <section className={styles.text}>
+                        <h1>Welcome</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Dolor nobis assumenda ad laboriosam ex facere consequuntur,
+                            quo, cupiditate architecto molestiae
+                            laudantium id ea eos perferendis illum aliquid magnam incidunt dolore.
+                        </p>
+                    </section>
+                    <img src={foto1} alt="foto-restaurante"/>
+                </section>
+                <section className={styles.content_corpo}>
+                    <img src={foto2} alt="foto-restaurante"/>
+                    <section className={styles.text}>
+                        <h1>Welcome</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Dolor nobis assumenda ad laboriosam ex facere consequuntur,
+                            quo, cupiditate architecto molestiae
+                            laudantium id ea eos perferendis illum aliquid magnam incidunt dolore.
+                        </p>
+                    </section>
+                </section>
+            </section>
         </section>
     )
 }
